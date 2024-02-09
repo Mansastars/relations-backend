f446b6cffb7fd74d755673254d66357e0b0741b4
rightguydaniel <rightguydaniel@gmail.com>
2024-02-09 17:46:57 +0100

    updated add user

--- src/controllers/dealControllers/createDealContact.ts
+++ src/controllers/dealControllers/createDealContact.ts
@@ -22,38 +22,43 @@ export const createDealContact = async (request: JwtPayload, response: Response)
     const contactId = v4();
     // const meeting = new Date()
     const findContact = (await Contact.findOne({
-      where: { email },
+      where: { email, deal_id, owner_id:userId },
     })) as unknown as ContactAttributes;
-
-
-    if(!findContact){
-    const newContact = await Contact.create({
-        id: contactId,
-        owner_id: userId,
-        deal_id,
-        first_name,
-        last_name,
-        organization_name: organization_name || `${first_name}`,
-        deal_size: deal_size || 0,
-        email: email,
-        phone_number: phone_number || '1234567890',
-        stage,
-        meeting_date: meeting_date || new Date(),
-        notes: notes || 'No notes available',
-        createdAt: new Date(),
-        updatedAt: new Date(),
-    });
-    return response.status(400).json({
-      status: `success`,
-      message: `Contact successfully created`,
-    });
-  }
     if (findContact) {
       return response.status(400).json({
         status: `error`,
         message: `${first_name} ${last_name} already exists on this deal`,
       });
     }
+    const newContact = await Contact.create({
+      id: contactId,
+      owner_id: userId,
+      deal_id,
+      first_name,
+      last_name,
+      organization_name: organization_name || `${first_name}`,
+      deal_size: deal_size || 0,
+      email: email,
+      phone_number: phone_number || '1234567890',
+      stage,
+      meeting_date: meeting_date || new Date(),
+      notes: notes || 'No notes available',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+  });
+  return response.status(200).json({
+    status: `success`,
+    message: `Contact successfully created`,
+  });
+
+    console.log(request.body)
+    if(!newContact){
+      return response.status(200).json({
+        status: `error`,
+        message: `unable to create contact`,
+      });
+  }
+  
   } catch (error: any) {
     console.log(error.message);
     return response.status(500).json({
