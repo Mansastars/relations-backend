2af7a128e729d2f88d654199598e458d8fd001fd
rightguydaniel <rightguydaniel@gmail.com>
2024-03-01 17:00:31 +0100

    quick fix

---
+++ migrations/added_trail_user.js
@@ -0,0 +1,21 @@
+'use strict';
+
+// /** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.addColumn('User', 'on_trial', {
+      type: Sequelize.DataTypes.STRING,
+      allowNull: true, // or false if the column should not allow null values
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.removeColumn('YourTableName', 'new_column_name');
+  }
+};
