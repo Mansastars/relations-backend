75c1cc5e9682a0a65aaf39e4dc832f667fe8f5be
rightguydaniel <rightguydaniel@gmail.com>
2024-03-20 14:56:27 +0100

    fix issue on all deals

--- src/controllers/dealControllers/getAllDeals.ts
+++ src/controllers/dealControllers/getAllDeals.ts
@@ -1,61 +1,56 @@
 import { JwtPayload } from "jsonwebtoken";
-import {Response} from 'express'
+import { Response } from 'express'
 import Deal from "../../models/dealModel/dealModel";
 import User, { UserAttributes } from "../../models/userModel/userModel";
 
-export const getAllDeals = async (request:JwtPayload ,response:Response) => {
-    try{
+export const getAllDeals = async (request: JwtPayload, response: Response) => {
+    try {
         const userId = request.user.id;
-        const deals = await Deal.findAll({where: {owner_id:userId}})
-        if (deals.length === 0){
-            return response.status(200).json({
-                status:"success",
-                message:"You currently have no deals",
-            })
-        }
-        
+        const deals = await Deal.findAll({ where: { owner_id: userId } })
 
-        const user = (await User.findOne({
-            where: { id: userId},
-          })) as unknown as UserAttributes;
-      
+        const user = (await User.findOne({ where: { id: userId }, })) as unknown as UserAttributes;
 
         //To calculate number of days from account creation
-    const currentDate:any = new Date()
-    const userCreatedDate: any = user.createdAt
-    const numberOfDays: any = Number((currentDate - userCreatedDate) / (1000 * 60 * 60 * 24))
-    const numberOfDaysLeft = 7 - numberOfDays
+        const currentDate: any = new Date()
+        const userCreatedDate: any = user.createdAt
+        const numberOfDays: any = Number((currentDate - userCreatedDate) / (1000 * 60 * 60 * 24))
+        const numberOfDaysLeft = 7 - numberOfDays
 
-    //to show banner
-    let showBanner = null
-    if(numberOfDays < 7 && user.subscription_name === null){
-      showBanner = true
-    }else{
-      showBanner = false
-    }
+        //to show banner
+        let showBanner = null
+        if (numberOfDays < 7 && user.subscription_name === null) {
+            showBanner = true
+        } else {
+            showBanner = false
+        }
 
-    if (deals.length > 0){
-        return response.status(200).json({
-            status:"success",
-            message:`You currently have ${deals.length} deals`,
-            deals,
-            showBanner,
-            user
-        })
-    }
-    return response.status(404).json({
-        status:"error",
-        message:"No deal found",
-        showBanner,
-        user
-    })
-        
+        if (deals.length > 0) {
+            return response.status(200).json({
+                status: "success",
+                message: `You currently have ${deals.length} deals`,
+                deals,
+                showBanner,
+                user
+            })
+        } else if (deals.length === 0) {
+            return response.status(200).json({
+                status: "success",
+                message: "No deal found",
+                showBanner,
+                user
+            })
+        } else {
+            return response.status(400).json({
+                status: "error",
+                message: "please try again",
+            })
+        }
 
-    }catch(error:any){
+    } catch (error: any) {
         console.log(error.message);
         return response.status(500).json({
-            status:"error",
-            message:"Internal Server Error"
+            status: "error",
+            message: "Internal Server Error"
         })
     }
 }
