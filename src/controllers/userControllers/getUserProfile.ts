c790e5b3dee8b0313f84d91c469a445e38683b8f
rightguydaniel <rightguydaniel@gmail.com>
2024-03-21 14:07:58 +0100

    fixed user profile

--- src/controllers/userControllers/getUserProfile.ts
+++ src/controllers/userControllers/getUserProfile.ts
@@ -1,17 +1,43 @@
 import { JwtPayload } from "jsonwebtoken";
 import {Response} from 'express'
-import User from "../../models/userModel/userModel";
+import User, { UserAttributes } from "../../models/userModel/userModel";
 
 export const getUserProfile = async (request:JwtPayload ,response:Response) => {
     try{
         const userId = request.user.id;
-        const user = await User.findOne({where: {id:userId}})
+        const user = await User.findOne({where: {id:userId}}) as unknown as UserAttributes;
         console.log(userId)
+
+
+
+        //To calculate number of days from account creation
+    const currentDate:any = new Date()
+    const userCreatedDate: any = user.createdAt
+    const numberOfDays: any = Number((currentDate - userCreatedDate) / (1000 * 60 * 60 * 24))
+    const numberOfDaysLeft = 7 - numberOfDays
+
+    //to show banner
+    let showBanner = null
+    if(numberOfDays < 7 && user.subscription_name === null){
+      showBanner = true
+    }else{
+      showBanner = false
+    }
+
+    //to show billing page
+    let showBilling = null
+    if(numberOfDays > 7 && user.subscription_name === null){
+        showBilling = true
+    }else{
+        showBilling = false
+    }
         if (user){
             return response.status(200).json({
                 status:"success",
                 message:"User Profile successful found",
-                data: user
+                user,
+                showBanner,
+                showBilling
             })
         }
         console.log(userId)
