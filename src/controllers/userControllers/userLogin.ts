27cc042a850d9d2bd0f1d58f438b9f3b876d478d
rightguydaniel <rightguydaniel@gmail.com>
2024-04-23 15:52:27 +0100

    fixed login page

--- src/controllers/userControllers/userLogin.ts
+++ src/controllers/userControllers/userLogin.ts
@@ -10,7 +10,7 @@ const stripe = require("stripe")(process.env.SECRET_KEY)
 
 export const userLogin = async (request: Request, response: Response) => {
   try {
-    let { email, password } = request.body;
+    const { email, password } = request.body;
 
     const user1 = (await User.findOne({ where: { email: email } })) as unknown as UserAttributes;
 
@@ -22,7 +22,6 @@ export const userLogin = async (request: Request, response: Response) => {
     }
 
     const validate = await bcrypt.compare(password, user1.password);
-
     if (!validate) {
       return response.status(400).json({
         status: `error`,
@@ -38,110 +37,84 @@ export const userLogin = async (request: Request, response: Response) => {
 
     //to check if customer is subscribed
     const customers = await stripe.customers.list({ email: email })
-    if (customers.data.length > 0) {
+    if(customers.data.length > 0){
       const Allsubscription = await stripe.subscriptions.list({ customer: customers.data[0].id, status: 'active' })
-      const session = await stripe.invoices.retrieve(Allsubscription.data[0].latest_invoice);
-      if (session.status === 'paid') {
-        const subscriptionId = session.subscription
-        const subscription = await stripe.subscriptions.retrieve(subscriptionId);
-        const planId = subscription.plan.id;
+      if(Allsubscription.data.length > 0){
+        const session = await stripe.invoices.retrieve(Allsubscription.data[0].latest_invoice);
+        if (session && session.status === 'paid') {
+          const subscriptionId = session.subscription
+          const subscription = await stripe.subscriptions.retrieve(subscriptionId);
+          const planId = subscription.plan.id;
 
-        let planType = ''
-        if (subscription.plan.amount === 2399) planType = "Early-Bird-Monthly"
-        if (subscription.plan.amount === 28800) planType = "Early-Bird-Yearly"
-        if (subscription.plan.amount === 1499) planType = "Basic-Monthly"
-        if (subscription.plan.amount === 17000) planType = "Basic-Yearly"
-        if (subscription.plan.amount === 2999) planType = "Standard-Monthly"
-        if (subscription.plan.amount === 30000) planType = "Standard-Yearly"
-        if (subscription.plan.amount === 9999) planType = "Premium-Monthly"
-        if (subscription.plan.amount === 100000) planType = "Premium-Yearly"
+          let planType = ''
+          if (subscription.plan.amount === 2399) planType = "Early-Bird-Monthly"
+          if (subscription.plan.amount === 28800) planType = "Early-Bird-Yearly"
+          if (subscription.plan.amount === 1499) planType = "Basic-Monthly"
+          if (subscription.plan.amount === 17000) planType = "Basic-Yearly"
+          if (subscription.plan.amount === 2999) planType = "Standard-Monthly"
+          if (subscription.plan.amount === 30000) planType = "Standard-Yearly"
+          if (subscription.plan.amount === 9999) planType = "Premium-Monthly"
+          if (subscription.plan.amount === 100000) planType = "Premium-Yearly"
 
-        const startDate = new Date(moment.unix(subscription.current_period_start).toDate().toISOString());
-        const endDate = new Date(moment.unix(subscription.current_period_end).toDate().toISOString());
-        const durationInSeconds = subscription.current_period_end - subscription.current_period_start;
-        const durationInDays = moment.duration(durationInSeconds, 'seconds').asDays();
-        const userPaymentExist = await Payment.findOne({ where: { user_id: user1.id } })
-        if (userPaymentExist) {
-          const updateUserPayment = await Payment.update({
-            session,
-            plan_id: planId,
-            plan_type: planType,
-            status: session.status,
-            plan_start_date: startDate,
-            plan_end_date: endDate,
-            plan_duration: String(durationInDays),
-            updatedAt: new Date(),
-          }, { where: { user_id: user1.id } })
-        } else {
-          const createUserPayment = await Payment.create({
-            id: v4(),
-            user_id: user1.id,
-            session,
-            session_id: session.id,
-            plan_id: planId,
-            plan_type: planType,
-            status: session.payment_status,
-            plan_start_date: startDate,
-            plan_end_date: endDate,
-            plan_duration: String(durationInDays),
-            createdAt: new Date(),
-            updatedAt: new Date(),
-          })
-        }
-
-        await User.update({
-          is_subscribed: true,
-          subscription_start_date: startDate,
-          subscription_end_date: endDate,
-          subscription_name: planType,
-          on_trial: false
-        }, {
-          where: {
-            id: user1.id
+          const startDate = new Date(moment.unix(subscription.current_period_start).toDate().toISOString());
+          const endDate = new Date(moment.unix(subscription.current_period_end).toDate().toISOString());
+          const durationInSeconds = subscription.current_period_end - subscription.current_period_start;
+          const durationInDays = moment.duration(durationInSeconds, 'seconds').asDays();
+          const userPaymentExist = await Payment.findOne({ where: { user_id: user1.id } })
+          if (userPaymentExist) {
+            const updateUserPayment = await Payment.update({
+              session,
+              plan_id: planId,
+              plan_type: planType,
+              status: session.status,
+              plan_start_date: startDate,
+              plan_end_date: endDate,
+              plan_duration: String(durationInDays),
+              updatedAt: new Date(),
+            }, { where: { user_id: user1.id } })
+          } else {
+            const createUserPayment = await Payment.create({
+              id: v4(),
+              user_id: user1.id,
+              session,
+              session_id: session.id,
+              plan_id: planId,
+              plan_type: planType,
+              status: session.payment_status,
+              plan_start_date: startDate,
+              plan_end_date: endDate,
+              plan_duration: String(durationInDays),
+              createdAt: new Date(),
+              updatedAt: new Date(),
+            })
           }
-        })
 
+          await User.update({
+            is_subscribed: true,
+            subscription_start_date: startDate,
+            subscription_end_date: endDate,
+            subscription_name: planType,
+            on_trial: false
+          }, {
+            where: {
+              id: user1.id
+            }
+          })
+        }else{
+
+        }
+      }else{
+      
       }
-    } else {
-      const user = (await User.findOne({ where: { email: email } })) as unknown as UserAttributes;
-
-      //To calculate number of days from account creation
-      const currentDate: any = new Date()
-      const userCreatedDate: any = user1.createdAt
-      const numberOfDays: any = Number((currentDate - userCreatedDate) / (1000 * 60 * 60 * 24))
-      const numberOfDaysLeft = 7 - numberOfDays
-
-      //to show banner
-      let showBanner = null
-      if (numberOfDays < 7 && user.subscription_name === null) {
-        showBanner = true
-      } else {
-        showBanner = false
-      }
-
-      //to show billing page
-      let showBilling = null
-      if (numberOfDays > 7 && user.subscription_name === null) {
-        showBilling = true
-      } else {
-        showBilling = false
-      }
-
-      return response.status(200).json({
-        message: `Welcome back ${user.first_name}`,
-        token,
-        user,
-        numberOfDaysLeft,
-        showBanner,
-        showBilling
-      });
+    }else{
+      
     }
 
     const user = (await User.findOne({ where: { email: email } })) as unknown as UserAttributes;
 
-    //To calculate number of days from account creation
+    // To calculate number of days from account creation
     const currentDate: any = new Date()
-    const userCreatedDate: any = user1.createdAt
+    const userCreatedDate: any = user.createdAt
     const numberOfDays: any = Number((currentDate - userCreatedDate) / (1000 * 60 * 60 * 24))
     const numberOfDaysLeft = 7 - numberOfDays
 
@@ -164,14 +137,13 @@ export const userLogin = async (request: Request, response: Response) => {
     return response.status(200).json({
       message: `Welcome back ${user.first_name}`,
       token,
-      user,
+      user: user1,
       numberOfDaysLeft,
       showBanner,
       showBilling
     });
-
   } catch (error: any) {
-    console.log(error.message);
+    console.log("Error:",error.message);
     response.status(500).json({
       status: `error`,
       method: request.method,
