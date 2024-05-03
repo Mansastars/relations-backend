5909b28a8a10745ad8296bd294e1ab3611a50cd9
rightguydaniel <rightguydaniel@gmail.com>
2024-05-03 22:41:19 +0100

    fixed get all contacts

--- src/controllers/contactControllers/getAllContact.ts
+++ src/controllers/contactControllers/getAllContact.ts
@@ -6,6 +6,17 @@ export const getAllContacts = async (request:JwtPayload ,response:Response) => {
     try{
         const userId = request.user.id;
         const allContacts = await GeneralContact.findAll({where: {owner_id:userId}})
+
+        const data:any = []
+        allContacts.map((item:any)=>{
+            const contact = {
+                first_name: item.first_name,
+                last_name: item.last_name,
+                organization_name: item.organization_name,
+                gender: item.gender,
+            }
+            data.push(contact)
+        })
         if (allContacts.length === 0){
             return response.status(200).json({
                 status:"success",
@@ -16,7 +27,7 @@ export const getAllContacts = async (request:JwtPayload ,response:Response) => {
             return response.status(200).json({
                 status:"success",
                 message:`You currently have ${allContacts.length} contacts`,
-                allContacts
+                data
             })
         }
         return response.status(404).json({
