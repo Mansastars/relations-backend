b459063a083ceea8e597f5eabee20d774fc80809
rightguydaniel <rightguydaniel@gmail.com>
2024-05-21 15:40:59 +0100

    fix token time

--- src/controllers/contactControllers/importContacts.ts
+++ src/controllers/contactControllers/importContacts.ts
@@ -1,8 +1,6 @@
 import { Response } from "express";
 import { JwtPayload } from "jsonwebtoken";
 import User from "../../models/userModel/userModel";
-import Contact from "../../models/contactModel/contactModel";
-import { where } from "sequelize";
 import { v4 } from "uuid";
 import GeneralContact from "../../models/generalContacts/generalContacts";
 
@@ -25,21 +23,21 @@ export const importContacts = async (request: JwtPayload, response: Response) =>
         }
 
         const imported = csv.map(async (contact: any) => {
-                await GeneralContact.create({
-                    title: contact.title || ``,
-                    id: v4(),
-                    owner_id: user,
-                    first_name: (contact.first_name || ``).toLowerCase(),
-                    last_name: (contact.last_name || ``).toLowerCase(),
-                    organization_name: contact.organization_name || ``,
-                    linkedin_url: contact.linkedin_url || ``,
-                    gender: contact.gender || ``,
-                    profile_pic: contact.profile_pic || ``,
-                    email: contact.email || ``,
-                    phone_number: contact.phone_number || ``,
-                    createdAt: new Date(),
-                    updatedAt: new Date(),
-                });
+            await GeneralContact.create({
+                title: contact.title || ``,
+                id: v4(),
+                owner_id: user,
+                first_name: (contact.first_name || ``).toLowerCase(),
+                last_name: (contact.last_name || ``).toLowerCase(),
+                organization_name: contact.organization_name || ``,
+                linkedin_url: contact.linkedin_url || ``,
+                gender: contact.gender || ``,
+                profile_pic: contact.profile_pic || ``,
+                email: contact.email || ``,
+                phone_number: contact.phone_number || ``,
+                createdAt: new Date(),
+                updatedAt: new Date(),
+            });
         })
         if (imported) {
             return response.status(200).json({
