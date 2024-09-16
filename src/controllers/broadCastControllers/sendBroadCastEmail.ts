7069c871e99854ce298ece26cfaf509da92dac55
rightguydaniel <rightguydaniel@gmail.com>
2024-09-16 08:03:21 +0100

    fix

---
+++ src/controllers/broadCastControllers/sendBroadCastEmail.ts
@@ -0,0 +1,47 @@
+import { Response } from "express";
+import { JwtPayload } from "jsonwebtoken";
+import GeneralContact from "../../models/generalContacts/generalContacts";
+import { template1 } from "../../utilities/notifications/broadCastMail/template1";
+
+export const sendBroadCastEmail = async (
+  request: JwtPayload,
+  response: Response
+) => {
+    const userId = request.user.id
+  const {
+    sender_email,
+    recipients_email,
+    subject,
+    email_content,
+    send_to_all,
+    name,
+    address,
+    phone_number,
+    logo
+  } = request.body;
+  try {
+    console.log(recipients_email)
+    console.log(request.body)
+    if(send_to_all == true){
+        const allContacts = await GeneralContact.findAll({where: {owner_id:userId}})
+        allContacts.map((contact)=>{
+            template1(sender_email, contact.email, subject, email_content, address, name, logo,phone_number)
+        })
+    }else{
+        const allContacts = recipients_email.split(' ')
+        console.log(allContacts)
+        allContacts.map((contact:any)=>{
+            template1(sender_email, contact, subject, email_content, address, name, logo,phone_number)
+        })
+    }
+    return response.status(200).json({
+        status: "success",
+        message: `Successfully Sent`,
+      });
+  } catch (error: any) {
+    return response.status(500).json({
+      status: "error",
+      message: `Internal Server Error`,
+    });
+  }
+};
